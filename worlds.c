73f98c26ce8d3d17c3b8dbf94a6b0d59d080feee
John Process <esologic@gmail.com>
2010-11-21 02:25:30 -0500

    worlds dialog completed

--- worlds.c
+++ worlds.c
@@ -16,7 +16,8 @@ enum columns {
 enum button_responses {
     ADD_WORLD = 1,
     REMOVE_WORLD,
-    CONNECT_TO_WORLD
+    CONNECT_TO_WORLD,
+    CANCEL
 };
 
 static void
@@ -70,7 +71,96 @@ read_and_display_from_config(GtkListStore *store)
 }
 
 static void
-dialog_response(GtkDialog *dialog,
+write_to_config(GtkListStore *store)
+{
+    char *home_dir = getenv("HOME");
+    int len = strlen(home_dir) + sizeof(WORLD_CONFIG_PATH) + 1;
+    char *fin_name = malloc(sizeof(*fin_name) * len);
+    snprintf(fin_name, len, "%s/%s", home_dir, WORLD_CONFIG_PATH);
+
+    FILE *fout = fopen(fin_name, "w");
+
+    GValue name_value = {0};
+    GValue hostname_value = {0};
+    GValue port_value = {0};
+    GtkTreeIter iter;
+    gboolean more = FALSE;
+
+    more = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(store), &iter);
+    while(more)
+    {
+        gtk_tree_model_get_value(GTK_TREE_MODEL(store),
+                                 &iter,
+                                 WORLD_NAME,
+                                 &name_value);
+        gtk_tree_model_get_value(GTK_TREE_MODEL(store),
+                                 &iter,
+                                 WORLD_HOSTNAME,
+                                 &hostname_value);
+        gtk_tree_model_get_value(GTK_TREE_MODEL(store),
+                                 &iter,
+                                 WORLD_PORT,
+                                 &port_value);
+
+        char *name = strdup(g_value_get_string(&name_value));
+        char *hostname = strdup(g_value_get_string(&hostname_value));
+        unsigned short port = g_value_get_int(&port_value);
+        
+        g_value_unset(&name_value);
+        g_value_unset(&hostname_value);
+        g_value_unset(&port_value);
+
+        fputs(name, fout);
+        fputc('\n', fout);
+        fputs(hostname, fout);
+        fputc('\n', fout);
+        char port_as_string[10];
+        snprintf(port_as_string, 10, "%d", port);
+        fputs(port_as_string, fout);
+        fputc('\n', fout);
+        
+        more = gtk_tree_model_iter_next(GTK_TREE_MODEL(store), &iter);
+    }
+        
+    fclose(fout);
+}
+
+static void
+add_world_response(GtkWidget *dialog,
+                   gint response_id,
+                   gpointer user_data)
+{
+    if(response_id == ADD_WORLD)
+    {
+        GtkWidget **widgets = user_data;
+        GtkWidget *tree = widgets[0];
+
+        GtkWidget *name_entry = widgets[1];
+        GtkWidget *hostname_entry = widgets[2];
+        GtkWidget *port_entry = widgets[3];
+        
+        const gchar *name = gtk_entry_get_text(GTK_ENTRY(name_entry));
+        const gchar *hostname = gtk_entry_get_text(GTK_ENTRY(hostname_entry));
+        const gchar *port = gtk_entry_get_text(GTK_ENTRY(port_entry));
+
+        GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(tree));
+
+        int port_int = strtoul(port, NULL, 10);
+
+        GtkTreeIter iter;
+        gtk_list_store_append(GTK_LIST_STORE(model), &iter);
+        gtk_list_store_set(GTK_LIST_STORE(model), &iter, 
+                           WORLD_NAME, name,
+                           WORLD_HOSTNAME, hostname,
+                           WORLD_PORT, port_int,
+                           -1);
+
+        write_to_config(GTK_LIST_STORE(model));
+    }
+}
+
+static void
+dialog_response(GtkWidget *dialog,
                 gint response_id,
                 gpointer user_data)
 {
@@ -155,7 +245,43 @@ dialog_response(GtkDialog *dialog,
     }
     case ADD_WORLD:
     {
-        /* TODO: finish this */
+        GtkWidget *dialog2 = gtk_dialog_new_with_buttons("Add World",
+                                                         GTK_WINDOW(MUDC.widgets.main_window),
+                                                         GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                         "Add", ADD_WORLD,
+                                                         "Cancel", CANCEL,
+                                                         NULL);
+
+        gtk_window_set_default_size(GTK_WINDOW(dialog2), 320, 200);
+
+        GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog2));
+
+        GtkWidget *ts1 = gtk_table_new(3, 2, FALSE);
+        gtk_table_set_row_spacings(GTK_TABLE(ts1), 10);
+        gtk_table_set_col_spacings(GTK_TABLE(ts1), 10);
+        gtk_box_pack_start(GTK_BOX(content_area), ts1, TRUE, FALSE, 0);
+
+        gtk_table_attach_defaults(GTK_TABLE(ts1), gtk_label_new("World Name"), 0, 1, 0, 1);
+        gtk_table_attach_defaults(GTK_TABLE(ts1), gtk_label_new("Hostname"), 0, 1, 1, 2);
+        gtk_table_attach_defaults(GTK_TABLE(ts1), gtk_label_new("Port"), 0, 1, 2, 3);
+
+        GtkWidget *name_entry = gtk_entry_new();
+        gtk_table_attach_defaults(GTK_TABLE(ts1), name_entry, 1, 2, 0, 1);
+        GtkWidget *hostname_entry = gtk_entry_new();
+        gtk_table_attach_defaults(GTK_TABLE(ts1), hostname_entry, 1, 2, 1, 2);
+        GtkWidget *port_entry = gtk_entry_new();
+        gtk_table_attach_defaults(GTK_TABLE(ts1), port_entry, 1, 2, 2, 3);
+        
+        GtkWidget *widgets[] = {user_data, name_entry, hostname_entry, port_entry};
+
+        g_signal_connect(dialog2, "response",
+                         G_CALLBACK(add_world_response),
+                         widgets);
+
+        gtk_widget_show_all(dialog2);
+        gtk_dialog_run(GTK_DIALOG(dialog2));
+        gtk_widget_destroy(dialog2);
+        
         break;
     }
     case REMOVE_WORLD:
@@ -169,6 +295,8 @@ dialog_response(GtkDialog *dialog,
         GtkTreeIter iter;        
         gtk_tree_model_get_iter(model, &iter, path);
         gtk_list_store_remove(GTK_LIST_STORE(model), &iter);
+
+        write_to_config(GTK_LIST_STORE(model));
 
         break;
     }
